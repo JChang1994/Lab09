4a0a0b0f06ff5bd2092b419af72c093572f944a7
jchang1994 <jchang1994@live.com>
2018-04-08 13:59:38 +0900

    Starting out

---
+++ src/main/java/edu/handong/csee/java/salesreporter/SalesAssociate.java
@@ -0,0 +1,19 @@
+package edu.handong.csee.java.salesreporter;
+
+public class SalesAssociate {
+	String name;
+	double sales;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getSales() {
+		return sales;
+	}
+	public void setSales(double sales) {
+		this.sales = sales;
+	}
+}
---
+++ src/main/java/edu/handong/csee/java/salesreporter/SalesReporter.java
@@ -0,0 +1,16 @@
+package edu.handong.csee.java.salesreporter;
+
+import java.util.Scanner;
+
+public class SalesReporter {
+	private double highestSales = 0.0;
+	private double averageSales = 0.0;
+	private SalesAssociate[] team;
+	private int numberOfAssociates = 0;
+	
+	public static void main(String[] args) {
+		System.out.println("Enter number of sales associates: ");
+		
+	}
+	
+}
